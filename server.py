b645eca54fa57a3e0be22d9746ae1f8760dc36e0
edel <edel.altares@gmail.com>
2017-11-10 16:29:33 -0700

    typo

--- server.py
+++ server.py
@@ -272,7 +272,7 @@ class Server:
                     self.send_msg(content, client)
 
                 self.STATE = "DONE"
-                sekf.send_msg("END", client)
+                self.send_msg("END", client)
 
         # error occured
         except IOError as error:
