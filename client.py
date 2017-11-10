0b5c9dbb95874d35d6565922b9b52d40f202db52
edelgrace <edel.altares@gmail.com>
2017-11-09 19:19:04 -0700

    Attempting authentication

--- client.py
+++ client.py
@@ -22,7 +22,10 @@ class Client:
     OPERATION = None
     FILENAME = None
     CIPHER = None
-    NONCE = None
+    CLI_SOCKET = None
+    STATE = "HANDSHAKE"
+    CHALLENGE = None
+    RESPONSE = None
 
     def parse(self):
         """ Parse the arguments """
@@ -30,26 +33,128 @@ class Client:
         parser = argparse.ArgumentParser()
 
         # required arguments
+        parser.add_argument('operation', choices=['read', 'write'])
+        parser.add_argument('filename')
+        parser.add_argument('host')
         parser.add_argument('port', type=int)
-        parser.add_argument('secretkey')
-        parser.add_argument('ip')
-        
+        parser.add_argument('cipher', choices=['null', 'aes256', 'aes128'])
+        parser.add_argument('key')
+
+        # parse the arguments
         arguments = parser.parse_args()
 
-        self.IP_ADDR = arguments.ip
+        # save variables
+        self.OPERATION = arguments.operation
+        self.FILENAME = arguments.filename
+        self.IP_ADDR = arguments.host
         self.PORT = arguments.port
-        self.SECRET_KEY = arguments.secretkey
+        self.CIPHER = arguments.cipher
+        self.SECRET_KEY = arguments.key
 
         return
+
 
     def setup(self):
         """ Setup the client """
 
+        # get parsed arguments
+        self.parse()
+
+        # setup the client socket
+        try:
+            self.CLI_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.CLI_SOCKET.connect((self.IP_ADDR, self.PORT))
+        
+        except Exception as e:
+            print(str(e))
+            sys.exit(0)
+
+        # start the handshake
+        self.handshake()
+
+        return
+
+    
+    def handshake(self):
+        """ Start the handshake process """
+
+        # send the cipher and nonce
+        nonce = uuid.uuid4().hex
+        msg = self.CIPHER + " " + nonce
+
+        self.CLI_SOCKET.send(bytearray(msg, "utf-8"))
+
+        self.STATE = "CHALLENGE"
+
+        return
+
+    def challenge(self, data):
+        """ Receive and send challenge to server """
+
+        # parse the data from the server
+        data = data.decode("utf-8").strip()
+        data = data.split(": ")
+
+        self.CHALLENGE = data[1]
+
+        # generate message to send to server
+        challenge = uuid.uuid4().hex
+        msg = "You have been challenged: " + challenge
+        self.CLI_SOCKET.send(bytearray(msg, "utf-8"))
+
+        # generate the response the server should reply with
+        response = challenge + self.SECRET_KEY
+        self.RESPONSE = hashlib.sha224(response.encode("utf-8")).hexdigest()
+
+        # change the state
+        self.STATE = "RESPONSE"
+
+    def response(self, data):
+        """ Receive data from the server """
+
+        # receive the data from the server
+        data = data.decode("utf-8").strip()
+        data = data.split(": ")
+        response = data[1]
+
+        # check if the response is correct
+        if response == self.RESPONSE:
+
+            # send response
+            msg = "OK"
+            self.CLI_SOCKET.send(bytearray(msg, "utf-8"))
+
+        else:
+            # send an error
+            msg = "ERROR: Wrong"
+
+            self.CLI_SOCKET.send(bytearray(msg, "utf-8"))
+        
 
     def run(self):
         """ Run the client """
 
         while True:
+            data = self.CLI_SOCKET.recv(1024)
+            
+            if data:
+                
+                print("DEBUG " + self.STATE)
+                print(data)
+
+                # check if handshake done
+                if self.STATE == "CHALLENGE":
+                    self.challenge(data)
+
+                # check if challenge send
+                elif self.STATE == "RESPONSE":
+                    self.response(data)
+
+            # no more data, close connection
+            else:
+                print("Disconnected")
+                self.CLI_SOCKET.close()
+
             return
 
 
@@ -64,4 +169,8 @@ def run():
 
 # run the program
 if __name__ == "__main__":
-    run()
+    try:
+        run()
+    except KeyboardInterrupt:
+        print("Disconnected")
+        sys.exit(0)
