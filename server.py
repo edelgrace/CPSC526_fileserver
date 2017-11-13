bf4eda66d9584a99cf34f1798cbb8793390bca5f
edel <edel.altares@gmail.com>
2017-11-12 21:47:19 -0700

    Debugging

--- server.py
+++ server.py
@@ -353,6 +353,7 @@ class Server:
                             else:
                                 content += str(padding) * int((padding-2)/3)
                                 content += "=="
+                            print(content)
 
                     if len(content) != 0:
                         self.send_msg(content, client)
